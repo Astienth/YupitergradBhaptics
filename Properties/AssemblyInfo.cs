d86f0479e0964f472c9864911e9aa7762290beb4
Astienth <astienth@gmail.com>
2022-03-25 22:12:57 +0100

    initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("YupitergradBhaptics")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("YupitergradBhaptics")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// This is necessary for MelonLoader to know what the mod is called, where it is
+// and for what game to load it. You can find the info for exact name and company
+// of the game in the MelonLoader log.
+[assembly: MelonInfo(typeof(YupitergradBhaptics.YupitergradBhaptics), "YupitergradBhaptics", "1.0", "Astien")]
+[assembly: MelonGame("Gamedust", "Yupitergrad")]
+
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("1a9443e4-8778-49ac-a6a5-698febfe1b2b")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ YupitergradBhaptics.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using MelonLoader;
+using HarmonyLib;
+using MyBhapticsTactsuit;
+using Game.Scripts.GrapplingHook;
+
+namespace YupitergradBhaptics
+{
+    public class YupitergradBhaptics : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            tactsuitVr = new TactsuitVR();
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+        }
+        
+        [HarmonyPatch(typeof(GrapplingHook), "TriggerUsed", new Type[] { })]
+        public class bhaptics_TriggerUsed
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.PlaybackHaptics("HeartBeat");
+            }
+        }
+    }
+}
